9f8b568f100139a355248ef761c908f0767a57bd
RkZi <raulbrejao@gmail.com>
2024-12-28 00:39:08 -0300

    api-gamixi

---
+++ apidb.sql
@@ -0,0 +1,324 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Tempo de geração: 04/05/2024 às 18:10
+-- Versão do servidor: 11.3.2-MariaDB-log
+-- Versão do PHP: 8.2.17
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `apidb`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `agents`
+--
+
+CREATE TABLE `agents` (
+  `id` int(11) NOT NULL,
+  `agentCode` varchar(50) DEFAULT NULL,
+  `saldo` float NOT NULL DEFAULT 0,
+  `agentToken` varchar(255) NOT NULL,
+  `secretKey` varchar(255) NOT NULL,
+  `probganho` varchar(50) DEFAULT '0',
+  `probbonus` varchar(10) DEFAULT '0',
+  `probganhortp` varchar(10) DEFAULT '0',
+  `probganhoinfluencer` varchar(10) DEFAULT '0',
+  `probbonusinfluencer` varchar(10) DEFAULT '0',
+  `probganhoaposta` varchar(10) DEFAULT '0',
+  `probganhosaldo` varchar(10) DEFAULT '0',
+  `callbackurl` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `bikineparadisejson`
+--
+
+CREATE TABLE `bikineparadisejson` (
+  `id` int(11) NOT NULL,
+  `json` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin 
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `butterflyblossomplayerjson`
+--
+
+CREATE TABLE `butterflyblossomplayerjson` (
+  `id` int(11) NOT NULL,
+  `json` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `calls`
+--
+
+CREATE TABLE `calls` (
+  `id` int(11) NOT NULL,
+  `iduser` int(11) NOT NULL,
+  `gamecode` varchar(255) NOT NULL,
+  `jsonname` varchar(255) NOT NULL DEFAULT '0',
+  `steps` int(11) DEFAULT NULL,
+  `bycall` varchar(255) DEFAULT NULL,
+  `aw` float DEFAULT 0,
+  `status` varchar(255) NOT NULL DEFAULT 'pending'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `doublefortunejson`
+--
+
+CREATE TABLE `doublefortunejson` (
+  `id` int(10) NOT NULL,
+  `json` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin 
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `dragontigerluckjson`
+--
+
+CREATE TABLE `dragontigerluckjson` (
+  `id` int(10) NOT NULL,
+  `json` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin 
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `fortunedragonplayerjson`
+--
+
+CREATE TABLE `fortunedragonplayerjson` (
+  `id` int(11) NOT NULL,
+  `json` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin 
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+-- ---- DE----------------------------------------------------
+
+--
+-- Estrutura para tabela `fortunemouseplayerjson`
+--
+
+CREATE TABLE `fortunemouseplayerjson` (
+  `id` int(11) NOT NULL,
+  `json` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_binFAULT 
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `fortuneoxrplayerjson`
+--
+
+CREATE TABLE `fortuneoxrplayerjson` (
+  `id` int(11) NOT NULL,
+  `json` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin 
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `fortunerabbitplayerjson`
+--
+
+CREATE TABLE `fortunerabbitplayerjson` (
+  `id` int(11) NOT NULL,
+  `json` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin 
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `fortunetigerplayerjson`
+--
+
+CREATE TABLE `fortunetigerplayerjson` (
+  `id` int(11) NOT NULL,
+  `json` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `ganeshagoldjson`
+--
+
+CREATE TABLE `ganeshagoldjson` (
+  `id` int(10) NOT NULL,
+  `json` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin 
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `jungledelightjson`
+--
+
+CREATE TABLE `jungledelightjson` (
+  `id` int(10) NOT NULL,
+  `json` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin 
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `token` varchar(255) NOT NULL DEFAULT '',
+  `atk` varchar(255) NOT NULL,
+  `saldo` float NOT NULL DEFAULT 0,
+  `valorapostado` float NOT NULL DEFAULT 0,
+  `valordebitado` float NOT NULL DEFAULT 0,
+  `valorganho` float NOT NULL DEFAULT 0,
+  `rtp` double NOT NULL DEFAULT 0,
+  `isinfluencer` float NOT NULL DEFAULT 0,
+  `agentid` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Índices para tabelas despejadas
+--
+
+--
+-- Índices de tabela `agents`
+--
+ALTER TABLE `agents`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id` (`id`);
+
+--
+-- Índices de tabela `bikineparadisejson`
+--
+ALTER TABLE `bikineparadisejson`
+  ADD PRIMARY KEY (`id`) USING BTREE,
+  ADD KEY `id` (`id`) USING BTREE;
+
+--
+-- Índices de tabela `butterflyblossomplayerjson`
+--
+ALTER TABLE `butterflyblossomplayerjson`
+  ADD PRIMARY KEY (`id`) USING BTREE,
+  ADD KEY `id` (`id`) USING BTREE;
+
+--
+-- Índices de tabela `calls`
+--
+ALTER TABLE `calls`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id` (`id`);
+
+--
+-- Índices de tabela `doublefortunejson`
+--
+ALTER TABLE `doublefortunejson`
+  ADD PRIMARY KEY (`id`) USING BTREE,
+  ADD KEY `id` (`id`) USING BTREE;
+
+--
+-- Índices de tabela `dragontigerluckjson`
+--
+ALTER TABLE `dragontigerluckjson`
+  ADD PRIMARY KEY (`id`) USING BTREE,
+  ADD KEY `id` (`id`) USING BTREE;
+
+--
+-- Índices de tabela `fortunedragonplayerjson`
+--
+ALTER TABLE `fortunedragonplayerjson`
+  ADD PRIMARY KEY (`id`) USING BTREE,
+  ADD KEY `id` (`id`) USING BTREE;
+
+--
+-- Índices de tabela `fortunemouseplayerjson`
+--
+ALTER TABLE `fortunemouseplayerjson`
+  ADD PRIMARY KEY (`id`) USING BTREE,
+  ADD KEY `id` (`id`) USING BTREE;
+
+--
+-- Índices de tabela `fortuneoxrplayerjson`
+--
+ALTER TABLE `fortuneoxrplayerjson`
+  ADD PRIMARY KEY (`id`) USING BTREE,
+  ADD KEY `id` (`id`) USING BTREE;
+
+--
+-- Índices de tabela `fortunerabbitplayerjson`
+--
+ALTER TABLE `fortunerabbitplayerjson`
+  ADD PRIMARY KEY (`id`) USING BTREE,
+  ADD KEY `id` (`id`) USING BTREE;
+
+--
+-- Índices de tabela `fortunetigerplayerjson`
+--
+ALTER TABLE `fortunetigerplayerjson`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id` (`id`);
+
+--
+-- Índices de tabela `ganeshagoldjson`
+--
+ALTER TABLE `ganeshagoldjson`
+  ADD PRIMARY KEY (`id`) USING BTREE,
+  ADD KEY `id` (`id`) USING BTREE;
+
+--
+-- Índices de tabela `jungledelightjson`
+--
+ALTER TABLE `jungledelightjson`
+  ADD PRIMARY KEY (`id`) USING BTREE,
+  ADD KEY `id` (`id`) USING BTREE;
+
+--
+-- Índices de tabela `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id` (`id`);
+
+--
+-- AUTO_INCREMENT para tabelas despejadas
+--
+
+--
+-- AUTO_INCREMENT de tabela `calls`
+--
+ALTER TABLE `calls`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
